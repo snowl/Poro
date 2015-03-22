9413c69021668aac4e38d926010c72ef2c241876
Snowl <d@viddiaz.com>
2015-03-22 12:18:11 +1100

    Generate runes for loginpacket on the fly

--- Poro/PoroLib/Data/DataLoader.cs
+++ Poro/PoroLib/Data/DataLoader.cs
@@ -20,6 +20,7 @@ namespace PoroLib.Data
         public List<Champions> Champions;
         public List<ChampionSkins> ChampionSkins;
         public List<TalentGroup> TalentTree;
+        public List<RuneSlot> RuneTree;
 
         private Dictionary<string, int> _masterySort = new Dictionary<string, int> { { "Offense", 1 }, { "Defense", 2 }, { "Utility", 3 } };
 
@@ -48,9 +49,9 @@ namespace PoroLib.Data
                 string RuneVersion = DDragonVersions["rune"].ToString();
                 string MasteryVersion = DDragonVersions["mastery"].ToString();
 
-                string RuneData = client.DownloadString(string.Format("http://ddragon.leagueoflegends.com/cdn/{0}/data/en_US/rune.json", RuneVersion));
                 string MasteryData = client.DownloadString(string.Format("http://ddragon.leagueoflegends.com/cdn/{0}/data/en_US/mastery.json", MasteryVersion));
 
+                #region Mastery loading
                 Masteries mData = JsonConvert.DeserializeObject<Masteries>(MasteryData);
                 TalentTree = new List<TalentGroup>();
                 foreach (KeyValuePair<string, List<List<MasteryLite>>> mastery in mData.tree)
@@ -63,11 +64,11 @@ namespace PoroLib.Data
                         Index = _masterySort[mastery.Key] - 1
                     };
 
-                    for (int i = 0; i < mastery.Value.Count; i++ )
+                    for (int i = 0; i < mastery.Value.Count; i++)
                     {
                         List<Talent> talentList = new List<Talent>();
                         List<MasteryLite> masteryList = mastery.Value[i];
-                        for (int j = 0; j < masteryList.Count; j++ )
+                        for (int j = 0; j < masteryList.Count; j++)
                         {
                             if (masteryList[j] == null)
                                 continue;
@@ -111,6 +112,83 @@ namespace PoroLib.Data
 
                     TalentTree.Add(group);
                 }
+                #endregion
+
+                #region Rune Loading
+                RuneTree = new List<RuneSlot>();
+
+                //This code is... bad. 
+                int Modifier = 0; //Skip 10, 20, 30
+                int Take = 3; //Take one each loop and it will increase what it starts from (1 - 2 - 3)
+                //Loop from 1-9 and do it 3 times to generate the red, yellow and blue runes
+                for (int i = 1; i <= 9; i++)
+                {
+                    //At 10, 20 and 30 you need to increment the min level.
+                    if ((i - 1) % 3 == 0 && i != 1)
+                    {
+                        Modifier += 1;
+                    }
+
+                    //The id goes past 9 so add the required amount that we have looped
+                    int IdAdd = (Math.Abs(Take - 3) * 10);
+                    //Take the amount that it has gone over
+                    if (Take != 3)
+                    {
+                        IdAdd -= Math.Abs(Take - 3);
+                    }
+
+                    RuneSlot slot = new RuneSlot()
+                    {
+                        Id = IdAdd + i,
+                        RuneType = new RuneType(),
+                        MinLevel = (3 * i + 1) - Take + Modifier
+                    };
+
+                    if (Take == 3)
+                    {
+                        slot.RuneType.Name = "Red";
+                        slot.RuneType.Id = 1;
+                    }
+                    else if (Take == 2)
+                    {
+                        slot.RuneType.Name = "Yellow";
+                        slot.RuneType.Id = 3;
+                    }
+                    else
+                    {
+                        slot.RuneType.Name = "Blue";
+                        slot.RuneType.Id = 5;
+                    }
+
+                    RuneTree.Add(slot);
+
+                    //Re do the loop, reset the modifier and take one from the take so the min level starts off one integer higher than last time
+                    if (i == 9 && Take > 1)
+                    {
+                        Take -= 1;
+                        i = 0;
+                        Modifier = 0;
+                    }
+                }
+
+                //Add black runes
+                for (int i = 1; i <= 3; i++)
+                {
+                    RuneSlot slot = new RuneSlot()
+                    {
+                        Id = 27 + i, //Start id from 27 since thats where we left off
+                        RuneType = new RuneType
+                        {
+                            Id = 7,
+                            Name = "Black"
+                        },
+                        MinLevel = 10 * i
+                    };
+
+                    RuneTree.Add(slot);
+                }
+
+                #endregion
             }
 
             Console.WriteLine("[LOG] Loaded League of Legends Data");
